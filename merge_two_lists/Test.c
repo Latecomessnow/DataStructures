6fff74a73fd612efcde5b764e41c4595ffb054f8
古慧 <2874233502@qq.com>
2022-05-22 17:12:13 +0800

    合并两个有序链表

---
+++ merge_two_lists/Test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode* head, * tail;
+    //给一个带哨兵位的头节点进行尾插
+    head = tail = (struct ListNode*)malloc(sizeof(struct ListNode));
+    tail->next = NULL;
+    while (list1 && list2)
+    {
+        if (list1->val < list2->val)
+        {
+            tail->next = list1;
+            tail = tail->next;
+            list1 = list1->next;
+        }
+        else
+        {
+            tail->next = list2;
+            tail = tail->next;
+            list2 = lsit2->next;
+        }
+    }
+    if (list1)
+    {
+        tail->next = list1;
+    }
+    if (list2)
+    {
+        tail->next = list2;
+    }
+    struct ListNode* newhead = head->next;
+    free(head);
+    return newhead;
+}
